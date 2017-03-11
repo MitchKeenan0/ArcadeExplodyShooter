d07622fa8a171a80c2eb5813feae66a1c0852b28
Mitchell Keenan <mitchkeenan0@gmail.com>
2017-03-10 19:51:00 -0500

    Add files via upload

---
+++ EnemyMovement.cs
@@ -0,0 +1,117 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyMovement : characterTemplate {
+
+	//public enum EnemyType { Fire, Air, Earth, Water };
+	public Transform target;
+	public float speedGrowth = 1.1f;
+	public float predictiveScalar = 2;
+    [SerializeField]
+	ElementalType.Element element;
+	//EnemyType type;
+	Vector3 goalPosition;
+	float timeAlive;
+	float moveSpeedActual;
+	float turnSpeedActual;
+
+	void OnEnable()
+	{
+		rb = GetComponent<Rigidbody>();
+		rb.velocity = Vector3.zero;
+		moveSpeedActual = moveSpeed + Random.Range(0, moveSpeed);
+		turnSpeedActual = turnSpeed;
+	}
+
+	void Start ()
+	{
+		if (!target)
+			target = GameObject.FindGameObjectWithTag("Player").transform;
+
+		rb = GetComponent<Rigidbody>();
+		moveSpeedActual = moveSpeed + Random.Range(0, moveSpeed/2);
+		turnSpeedActual = turnSpeed;
+	}
+	
+	
+	void Update ()
+	{
+		// Increase Speed and Turn
+		timeAlive += Time.deltaTime;
+		moveSpeedActual += Time.deltaTime * speedGrowth;
+		turnSpeedActual += Time.deltaTime * speedGrowth;
+
+		// Track player, predict movement
+		if (target)
+		{
+			goalPosition = target.GetComponent<Player>().GetPlayerFuturePos(predictiveScalar);
+			ChasePlayer();
+		}
+		
+	}
+
+
+	void ChasePlayer()
+	{
+		// Rotate to Target
+		var dir = goalPosition - transform.position;
+		Quaternion rotToTarget = Quaternion.LookRotation(dir);
+		transform.rotation = Quaternion.Lerp(transform.rotation, 
+											 rotToTarget,
+											 Time.deltaTime * turnSpeed);
+
+		// Forward Propulsion
+		rb.velocity = transform.forward * moveSpeedActual;
+	}
+
+
+	void OnCollisionEnter(Collision c)
+	{
+		switch(c.gameObject.tag)
+		{
+			case "Player":
+				Explode(false);
+				UI_Manager.ResetComboPoints();
+				break;
+			//case "Wave":
+            case "Bullet":
+				checkType(c.gameObject);
+				break;
+		}
+	}
+
+
+	public void Explode(bool destroyedByPlayer)
+	{
+		GameObject newWave = Instantiate(Wave, 
+										transform.position + Vector3.up * (transform.localScale.y / 2),
+										Quaternion.identity);
+		if (newWave)
+		{
+			newWave.GetComponent<WaveForm>().destroyedByPlayer = destroyedByPlayer;
+			
+			if(destroyedByPlayer)
+			{
+				UI_Manager.UpdateComboPoints();
+                EndGameMenu.score += 1; 
+            }
+            newWave.GetComponent<WaveForm>().SetElement(element);
+			gameObject.SetActive(false);
+		}
+	}
+
+    public void checkType(GameObject c)
+    {
+        if (element == c.gameObject.GetComponent<bulletTemplate>().elementType)
+		{
+            Explode(true);
+        }
+    }
+
+	public new ElementalType.Element GetType()
+	{
+		return element;
+	}
+
+
+}
---
+++ WaveForm.cs
@@ -0,0 +1,93 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WaveForm : MonoBehaviour {
+
+	public bool destroyedByPlayer;
+	public float lifeTime = 2.0f;
+	public float waveSpeed = 1.0f;
+	public float waveRange = 1.0f;
+	public float killCheckInterval = 0.5f;
+	GameObject waveParticles;
+	static GameObject[] particleArray = new GameObject[4];
+
+	float timer = 0.0f;
+	float waveSize = 0.0f;
+	float timeAlive = 0.0f;
+	ElementalType.Element elementalType;
+
+	void Awake()
+	{
+		if (particleArray[3] == null)
+		{
+			for (int i = 0; i < particleArray.Length; i++)
+			{
+				particleArray[i] = Resources.Load(("Prefabs/" + ((ElementalType.Element)i).ToString() + "WaveParticles")) as GameObject;
+			}
+		}
+	}
+
+	void Start ()
+	{
+		timer = killCheckInterval / 2;
+	}
+	
+	
+	void Update ()
+	{
+		// Wave Propagation
+		waveSize = Mathf.Lerp(waveSize, waveRange, Time.deltaTime * (waveSpeed / 2));
+		
+		if (destroyedByPlayer)
+		{
+			// Kill Check
+			timer += Time.deltaTime;
+			if (timer >= (killCheckInterval / 1))
+			{
+				Collider[] cols = Physics.OverlapSphere(transform.position, waveSize);
+				foreach (Collider c in cols)
+				{
+					if (c.GetComponentInParent<EnemyMovement>())
+					{
+						if (c.gameObject.GetComponentInParent<EnemyMovement>().GetType() == elementalType)
+						{
+							c.GetComponentInParent<EnemyMovement>().Explode(true);
+						}
+					}
+				}
+				timer = 0.0f;
+			}
+		}
+		
+
+		// Lifespan / Destroy
+		timeAlive += Time.deltaTime;
+		if (timeAlive >= lifeTime)
+		{
+			Destroy(gameObject);
+		}
+
+		if (waveSize >= waveRange - 0.5f)
+		{
+			Destroy(gameObject);
+		}
+	}
+
+	void SpawnParticleSystem()
+	{
+		if (!waveParticles)
+		{
+			waveParticles = Instantiate(particleArray[(int)elementalType],
+									gameObject.transform.position,
+									Quaternion.Euler(-90, 0, 0),
+									this.transform);
+		}
+		
+	}
+
+	public void SetElement(ElementalType.Element newType)
+	{
+		elementalType = newType;
+		SpawnParticleSystem();
+	}
+}
